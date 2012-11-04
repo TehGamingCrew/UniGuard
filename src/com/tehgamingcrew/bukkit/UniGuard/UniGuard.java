89731293af612bfe5f1162a1df67b4b520070d18
Jasper9041 <Jasper9041@tehgamingcrew.com>
2012-11-04 22:01:10 +0100

    Basic standart stuff added

--- src/com/tehgamingcrew/bukkit/UniGuard/UniGuard.java
+++ src/com/tehgamingcrew/bukkit/UniGuard/UniGuard.java
@@ -1,7 +1,35 @@
 package com.tehgamingcrew.bukkit.UniGuard;
 
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class UniGuard extends JavaPlugin{
-	
+
+public class UniGuard extends JavaPlugin {
+
+	// external classes
+	public methods methods;
+
+	// Standard Necessary Vars
+	public PluginManager pm;
+	public PluginDescriptionFile pdFile;
+	public Logger log;
+
+	@Override
+	public void onEnable() {
+		methods = new methods(this);
+
+		methods.initDefVars();
+		methods.setCommands();
+
+
+		methods.sendEnabledMessage();
+	}
+
+	@Override
+	public void onDisable() {
+		methods.sendDisabledMessage();
+	}
 }
